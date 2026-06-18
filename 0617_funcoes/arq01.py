ca7b036bb3afe5ce4b8e82d4b752a40b203da89a
Matheus-svg1 <matheus01estudos@gmail.com>
2026-06-17 22:23:09 -0300

    arq01.py
    primeira aula criando códigos no vs code

---
+++ 0617_funcoes/arq01.py
@@ -0,0 +1,32 @@
+def somar_valores(parametro1, parametro2):
+    """Docstring: descreve o que a função faz"""
+    # Corpo da função
+    resultado = parametro1 + parametro2
+    return resultado
+
+soma = somar_valores(2, 3)
+print(soma)
+def dividir_valores(parametro1, parametro2):
+    """Docstring: descreve o que a função faz"""
+    # Corpo da função
+    resultado = parametro1 / parametro2 if parametro2 != 0 else "Erro: divisão por zero"
+    return resultado
+divisao = dividir_valores(1000 , 55)
+print(divisao)
+def multiplicar_valores(parametro1, parametro2, parametro3):
+    multiplicacao = ( 455, 6677, 866)
+    print(multiplicacao)
+    return resultado
+
+
+
+
+
+
+
+
+def imprime_nome(nome):
+    print("Nome:", nome)
+    # sem return explícito
+resultado = imprime_nome("Ana")
+print(resultado)
