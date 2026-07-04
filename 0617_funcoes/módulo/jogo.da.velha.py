76bcd06f4f85c6b3e232ca7022b930423c1f3341
Matheus-svg1 <matheus01estudos@gmail.com>
2026-07-03 22:06:18 -0300

    Update jogo.da.velha.py

--- 0617_funcoes/módulo/jogo.da.velha.py
+++ 0617_funcoes/módulo/jogo.da.velha.py
@@ -1,7 +1,8 @@
 
 linhas = 3
 colunas = 3
-jogador= "X"
+jogador_atual = "Jogador 1"
+simbolo = "X"
 tabuleiro = []
 jogadas = 0
 
@@ -10,56 +11,57 @@ for p in range(linhas):
     for g in range(colunas):
         linha.append(" ")
     tabuleiro.append(linha)
-
+print(f"Vez do {jogador_atual} ({simbolo})")
 while True:
     for i in range(3):
         print(" | ".join(tabuleiro[i]))
-    if i < 2:
-        print("---------")
+        if i < 2:
+            print("-" * 9)
+        print()
+
     coluna_escolhida = int(input("Escolha uma coluna de (0 a 2): "))
     linha_escolhida = int(input("Escolha uma linha de (0 a 2): "))
+    
+    #função para verificar vitória
 
     if linha_escolhida < 0 or linha_escolhida > 2 or coluna_escolhida < 0 or coluna_escolhida >2:
         print("Posição inválida!")
         continue
 
     if  tabuleiro[linha_escolhida][coluna_escolhida] == " ":
-        tabuleiro[linha_escolhida][coluna_escolhida] = jogador
+        tabuleiro[linha_escolhida][coluna_escolhida] = simbolo
         jogadas += 1
     else:
         print("Esta posição ja está ocupada")       
     for p in range(linhas):
-    
-        for g in range(colunas):
-            print(tabuleiro[p][g], end=" ")
-        print()
 
     #LINHAS
-    for i in range(3):
-        if tabuleiro[i][0] == jogador and tabuleiro[i][1] == jogador and tabuleiro[i][2]== jogador:
-            print(f"O jogador {jogador} venceu!")
-            exit()
+        for i in range(3):
+            if tabuleiro[i][0] == jogador_atual and tabuleiro[i][1] == jogador_atual and tabuleiro[i][2]== jogador_atual:
+                print(f"O jogador {jogador_atual} venceu!")
+                exit()
     #COLUNAS
     for i in range(3):
-        if tabuleiro[0][i] == jogador and tabuleiro[1][i] == jogador and tabuleiro[2][i] == jogador:
-            print(f"O jogador {jogador} venceu!")
+        if tabuleiro[0][i] == jogador_atual and tabuleiro[1][i] == jogador_atual and tabuleiro[2][i] == jogador_atual:
+            print(f"O jogador {jogador_atual} venceu!")
             exit()
      #DIAGONAL PRINCIPAL   
-        if tabuleiro[0][0] == jogador and tabuleiro[1][1] == jogador and tabuleiro[2][2] == jogador:
-            print(f"O jogador {jogador} venceu!")  
-            exit()
+    if tabuleiro[0][0] == jogador_atual and tabuleiro[1][1] == jogador_atual and tabuleiro[2][2] == jogador_atual:
+        print(f"O jogador {jogador_atual} venceu!")  
+        exit()
     #DIAGONAL SECUNDÁRIA    
-        if tabuleiro[0][2] == jogador and tabuleiro[1][1] == jogador and tabuleiro[2][0] == jogador:
-            print(f"O jogador {jogador} venceu!")
-            exit()
+    if tabuleiro[0][2] == jogador_atual and tabuleiro[1][1] == jogador_atual and tabuleiro[2][0] == jogador_atual:
+        print(f"O jogador {jogador_atual} venceu!")
+        exit()
      #EMPATE   
     if jogadas == 9:
         print("Empate!")
         exit() 
     #TROCA DE JOGADOR
-    if jogador == "X":
-        jogador = "O"
+    if jogador_atual == "Jogador 1":
+        jogador_atual = "Jogador 2"
+        simbolo = "O"
     else:
-        jogador = "X"
-
+        jogador_atual = "Jogador 1"
+        simbolo = "X"
                 
