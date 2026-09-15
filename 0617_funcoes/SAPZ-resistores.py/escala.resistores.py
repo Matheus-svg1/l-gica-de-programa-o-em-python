160456b3558408719f85d78cbb4ed4774888cd45
Matheus-svg1 <matheus01estudos@gmail.com>
2026-09-14 22:23:58 -0300

    sapz.py

--- 0617_funcoes/SAPZ-resistores.py/escala.resistores.py
+++ 0617_funcoes/SAPZ-resistores.py/escala.resistores.py
@@ -4,15 +4,15 @@ root=tk.Tk()
 def escalas(ohms):
     #Menor ou igual a 999
     if ohms <= 999:
-        return f"{ohms} Homns",
+        return f"{ohms} Ω",
     #Maior que 999 homs e menor ou igual a 99.999 (99999)
     elif 999 < ohms <=99999:
         kilo_ohms = ohms / 1000
-        return f"{kilo_ohms}Kilohoms (kΩ)"
+        return f"{kilo_ohms:.1f}(kΩ)"
     #Valores acima de 99.999
     else:
-        mega_homs = 100000
-        return f"{mega_homs}Megahoms(MΩ)"
+        mega_homs = ohms / 10000
+        return f"{mega_homs}(MΩ)"
 
 
 # Exemplos de teste:
