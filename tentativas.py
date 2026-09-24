23a8702b93485f583760d8d84dbfc10fb45cd7dc
Matheus-svg1 <matheus01estudos@gmail.com>
2026-09-24 17:31:26 -0300

    Update tentativas.py

---
+++ tentativas.py
@@ -0,0 +1,8 @@
+preco = float(input("Digite o valor do produto em R$:"))
+
+desconto = int(input("Digite o desconto do produto em %:"))
+
+
+sobra =  preco - desconto
+
+print(f"O valor em R$ do produto é {sobra}")
